87f8832d7c6a025f202b83b755b9b246a746c92f
Damian Mastylo <dmastylo@gmail.com>
2015-03-01 16:37:16 -0500

    Add peristence of usernames

--- sockets/base.js
+++ sockets/base.js
@@ -1,13 +1,28 @@
 module.exports = function(io) {
-  io.sockets.on('connection', function (socket) {
-    console.log('a user connected');
+  var clients = [];
 
-    socket.on('disconnect', function(){
+  io.sockets.on('connection', function (client) {
+    console.log('a user connected');
+    clients.push(client);
+
+    // Send already connected usernames to newly connected client
+    usernames = [];
+    for (var i = 0; i < clients.length; ++i) {
+      usernames.push(clients[i].username);
+    }
+    usernames = usernames.filter(function(n) { return n != undefined });
+    if (usernames.length) {
+      client.emit('new-username', usernames.join(','));
+    }
+
+    client.on('disconnect', function() {
       console.log('user disconnected');
+      clients.splice(clients.indexOf(client), 1);
     });
 
-    socket.on('username-registered', function(msg){
+    client.on('username-registered', function(msg) {
       console.log('username: ' + msg);
+      clients[clients.indexOf(client)].username = msg;
       io.emit('new-username', msg);
     });
   });
