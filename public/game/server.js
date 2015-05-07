fbc281de7805c2e45fbfff8f3685b7db3a078204
Max Llewellyn <max.llewellyn@gmail.com>
2015-05-07 17:02:52 -0400

    Allow games with more than 4 players to start.

--- public/game/server.js
+++ public/game/server.js
@@ -12,9 +12,9 @@ var Server = IgeClass.extend({
 		this.red_score = 0;
 		this.blue_score = 0;
 
-		this.game_length = 45;
+		this.game_length = 120;
 		this.game_time_left = 0;
-		this.required_players = 1;
+		this.required_players = 2;
 		// this is true when the game is being played and false when people are
 		// readying up
 		this.game_active = false;
@@ -132,8 +132,8 @@ var Server = IgeClass.extend({
 								return;
 							}
 							// check that there are the right number of players on each team
-							if (self.red_team_players != self.required_players ||
-								self.blue_team_players != self.required_players) {
+							if (self.red_team_players >= self.required_players ||
+								self.blue_team_players >= self.required_players) {
 								return;
 							}
 
